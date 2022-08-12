709f2ce9bdc6cd1f2ff2d8b27389d75822bc89ce
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2022-08-12 22:36:28 +0700

    Booking module

---
+++ src/booking.ts
@@ -0,0 +1,55 @@
+import { DataSnapshot } from "firebase-admin/database";
+import { handleError } from "./utilities";
+import * as database from "./firebase/database";
+import * as firestore from "./firebase/firestore";
+import { Account, AccountType } from "./account/account";
+import { FieldValue } from "firebase-admin/firestore";
+
+console.log("Listening for booking");
+
+export function enable()
+{
+    database.on("booking", "value", handleData);
+}
+
+export function disable()
+{
+    database.off("booking", "value");
+}
+
+async function handleData(snapshot: DataSnapshot)
+{
+    let val = snapshot.val();
+
+    if (!val)
+        return;
+
+    console.log("New booking from " + val.phoneNumber);
+
+    let account = await Account.GetByPhone(val.phoneNumber);
+
+    try {
+        if (account == null) {
+            await firestore.collection("users").add({
+                name: "Temporary user",
+                type: AccountType[AccountType.Customer],
+                phoneNumber: val.phoneNumber,
+                history: FieldValue.arrayUnion(val.endPoint.name)
+            })
+            console.log("Created temporary user " + val.phoneNumber);
+        }
+        else {
+            await firestore.collection("users").doc(account.id).update({
+                history: FieldValue.arrayUnion(val.endPoint.name)
+            })
+            console.log("Added to " + val.phoneNumber + " history");
+        }
+    }
+    catch (err) {
+        let errorString = String(err);
+        console.log(errorString);
+        database.ref("bookingStatus").set({
+            error: errorString
+        })
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as signUp from "./account/signUp"
 import * as login from "./account/login"
 import * as logout from "./account/logout"
+import * as booking from "./booking"
 import * as express from "express"
 
 let app = express();
@@ -9,6 +10,7 @@ app.listen(process.env.PORT || 5000, () => {
     signUp.enable();
     login.enable();
     logout.enable();
+    booking.enable();
 })
 
 app.get("/", (req, res) => {
